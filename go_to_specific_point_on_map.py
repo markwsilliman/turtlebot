c0ed3e91b95127f316c87bfec6471ec84c2a412f
Nadya Ampilogova <namp1@yandex.ru>
2016-01-24 06:23:30 +0300

    Added follow the route example

--- go_to_specific_point_on_map.py
+++ go_to_specific_point_on_map.py
@@ -15,60 +15,78 @@ Redistribution and use in source and binary forms, with or without modification,
 THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 '''
 
-#TurtleBot must have minimal.launch & amcl_demo.launch running prior to starting this script.
+# TurtleBot must have minimal.launch & amcl_demo.launch
+# running prior to starting this script
+# For simulation: launch gazebo world & amcl_demo prior to run this script
 
 import rospy
 from move_base_msgs.msg import MoveBaseAction, MoveBaseGoal
 import actionlib
 from actionlib_msgs.msg import *
-from geometry_msgs.msg import Pose, PoseWithCovarianceStamped, Point, Quaternion, Twist
+from geometry_msgs.msg import Pose, Point, Quaternion
 
 class GoToPose():
     def __init__(self):
-        rospy.init_node('nav_test', anonymous=False)
 
-	#what to do if shut down (e.g. ctrl + C or failure)
+	# What to do if shut down (e.g. Ctrl + C or failure)
 	rospy.on_shutdown(self.shutdown)
-
 	
-	#tell the action client that we want to spin a thread by default
+	# Tell the action client that we want to spin a thread by default
 	self.move_base = actionlib.SimpleActionClient("move_base", MoveBaseAction)
-	rospy.loginfo("wait for the action server to come up")
-	#allow up to 5 seconds for the action server to come up
+	rospy.loginfo("Wait for the action server to come up")
+
+	# Allow up to 5 seconds for the action server to come up
 	self.move_base.wait_for_server(rospy.Duration(5))
 
-	#we'll send a goal to the robot to tell it to move to a pose that's near the docking station
+    def goto(self, pos, quat):
+
+        # Send a goal
 	goal = MoveBaseGoal()
 	goal.target_pose.header.frame_id = 'map'
 	goal.target_pose.header.stamp = rospy.Time.now()
-	#customize the following Point() values so they are appropriate for your location
-	goal.target_pose.pose = Pose(Point(2.18, -1.7, 0.000), Quaternion(0.000, 0.000, 0.892, -1.500))
+        goal.target_pose.pose = Pose(Point(pos['x'], pos['y'], 0.000),
+                                     Quaternion(quat['r1'], quat['r2'], quat['r3'], quat['r4']))
 
-	#start moving
+	# Start moving
         self.move_base.send_goal(goal)
 
-	#allow TurtleBot up to 60 seconds to complete task
+	# Allow TurtleBot up to 60 seconds to complete task
 	success = self.move_base.wait_for_result(rospy.Duration(60)) 
 
-
-	if not success:
-                self.move_base.cancel_goal()
-                rospy.loginfo("The base failed to reach the desired pose")
-    	else:
-		# We made it!
-		state = self.move_base.get_state()
-		if state == GoalStatus.SUCCEEDED:
-		    rospy.loginfo("Hooray, reached the desired pose")
-
-
+        if not success:
+            self.move_base.cancel_goal()
+            return False
+        else:
+            state = self.move_base.get_state()
+            if state == GoalStatus.SUCCEEDED:
+                # We made it!
+                return True
+            else:
+                return False
 
     def shutdown(self):
         rospy.loginfo("Stop")
-
+        rospy.sleep(1)
 
 if __name__ == '__main__':
     try:
-        GoToPose()
+        rospy.init_node('nav_test', anonymous=False)
+        navigator = GoToPose()
+
+        # Customize the following values so they are appropriate for your location
+        position = {'x': 1.22, 'y' : 2.56}
+        quaternion = {'r1' : 0.000, 'r2' : 0.000, 'r3' : 0.000, 'r4' : 1.000}
+
+        success = navigator.goto(position, quaternion)
+
+        if success:
+            rospy.loginfo("Hooray, reached the desired pose")
+        else:
+            rospy.loginfo("The base failed to reach the desired pose")
+
+        # Sleep to give the last log messages time to be sent
+        rospy.sleep(1)
+
     except rospy.ROSInterruptException:
         rospy.loginfo("Exception thrown")
 
